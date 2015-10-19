5d50fae6ff9faf55071e8f8f10e557f3ea39d9c5
yongbingao <ben.gao25@gmail.com>
2015-10-18 22:24:13 -0400

    locationTrivia lab done

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,6 +10,55 @@
     return YES;
 }
 
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    NSString *shortenLocationName = @"";
+    if ([location[@"name"] length]<length) {
+        shortenLocationName = location[@"name"];
+    }
+    else {
+        shortenLocationName = [[location valueForKey:@"name"] substringToIndex:length];
+    }
+    return shortenLocationName;
+}
+
+- (NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    NSDictionary *dictionaryWithNameLatLong
+    = @{@"name":name,
+        @"latitude":@(latitude),
+        @"longitude":@(longitude)};
+    return dictionaryWithNameLatLong;
+}
+
+- (NSArray *)namesOfLocations:(NSArray *)locations {
+    NSMutableArray *arrayOfLocations = [[NSMutableArray alloc]init];
+    for (NSDictionary *dictionaryOfLocations in locations) {
+        [arrayOfLocations addObject:dictionaryOfLocations[@"name"]];
+    }
+    return arrayOfLocations;
+}
+
+- (BOOL) dictionaryIsValidLocation:(NSDictionary *)location {
+    if (!([location allKeys].count ==3)){
+        return NO;
+    }
+    for (NSString *key in location) {
+        if (![key isEqualToString: @"name"]&&![key isEqualToString: @"latitude"]&&![key isEqualToString: @"longitude"]) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    NSDictionary *matchingNameDic = @{};
+    for (NSDictionary *locationInfo in locations) {
+        if ([locationInfo[@"name"] isEqualToString:name]) {
+            matchingNameDic=locationInfo;
+        }
+    }
+    return matchingNameDic;
+}
+
 /** 
  
  *  Write your method implementations here.
