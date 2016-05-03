5afbcc1c3523f19177a79272a02344abfaa15b44
John Richardson <richardsonjm@gmail.com>
2016-05-02 21:33:46 -0400

    Short name

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,30 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+- (NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length {
+    NSString *name = location[@"name"];
+    if ([name length] < length) {
+        return name;
+    } else {
+        NSString *shortName = [name substringToIndex:length];
+        return shortName;
+    }
+}
+
+- (NSString *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    return nil;
+}
+
+- (NSArray *)namesOfLocations:(NSArray *)locations {
+    return nil;
+}
+
+- (BOOL)dictionaryIsValidLocation:(NSDictionary *)dictionary {
+    return nil;
+}
+
+- (NSDictionary *)locationNamed:(NSString *)name inLocations:(NSArray *)locations {
+    return nil;
+}
 
 @end
